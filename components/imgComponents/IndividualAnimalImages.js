5d9d237f72d6a8d28888415e3db6862f6c7a9d5f
Vihaa-T <160472877+Vihaa-T@users.noreply.github.com>
2024-12-24 23:39:57 +0530

    Update IndividualAnimalImages.js

--- components/imgComponents/IndividualAnimalImages.js
+++ components/imgComponents/IndividualAnimalImages.js
@@ -2,16 +2,27 @@ import React from 'react';
 import Image from 'next/image';
 
 const IndividualAnimalImages = ({ result }) => {
-	// const getIndividualPhotos = () => {
-	// 	const selectedImages = [];
-	// 		for (const image of result.photos) {
-	// 			selectedImages.push(image.full);
-	// 		}
-	// 	return selectedImages;
-	// };
-	// const photos = getIndividualPhotos();
-	// console.log(photos);
+  // Assuming `result.photos` is an array of image objects and each object has a 'full' URL and a 'alt' text
+  const photos = result.photos || []; // Make sure photos exists, otherwise it defaults to an empty array
 
-	return <Image layout="intrinsic" src={result} width={500} height={500} />;
+  return (
+    <>
+      {photos.length > 0 ? (
+        photos.map((photo, index) => (
+          <Image
+            key={index}
+            layout="intrinsic"
+            src={photo.full}
+            alt={`Image of ${result.name} - Photo ${index + 1}`}
+            width={500}
+            height={500}
+          />
+        ))
+      ) : (
+        <p>No photos available</p>
+      )}
+    </>
+  );
 };
+
 export default IndividualAnimalImages;
